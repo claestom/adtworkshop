40b3c5e7de68bd628923365820ac381eb73096a7
claestom <99192568+claestom@users.noreply.github.com>
2023-03-09 23:42:56 +0100

    Add files via upload

---
+++ iotdevice.py
@@ -0,0 +1,54 @@
+import time
+import json
+from azure.iot.device import IoTHubDeviceClient, Message
+import random as r
+
+def get_temp():
+    return r.randint(16,25)
+
+def get_humidity():
+    return r.randint(1,120)
+
+def get_people():
+    return r.randint(0,580)
+
+def get_memory():
+    return r.randint(1,100)
+
+def get_cpu():
+    return r.randint(1,100)
+
+connection_string = "HostName=iothubpocCG.azure-devices.net;DeviceId=pocCG;SharedAccessKey=+04tDLpk2UfOaaatBSjLtU2sFKoMD3HiyLpfTh6IdaI="
+device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+print('Connecting')
+device_client.connect()
+print('Connected')
+
+
+def simulate_messages():
+
+    message = Message(json.dumps({
+    "eventTopic": "WalloniaDT",
+    "eventType": "READOUT",
+    "data":{
+    "Temperature": get_temp(),
+    "Humidity": get_humidity(),
+    "nPeople": get_people(),
+    "Memory": get_memory,
+    "CPU": get_cpu()
+    }}
+    ))
+
+    message.content_encoding = "utf-8"
+    message.content_type = "application/json"
+
+    return message
+
+while True:
+
+    message = simulate_messages()
+
+    device_client.send_message(message)
+    print("Simulated data: sent")
+
+    time.sleep(10)
